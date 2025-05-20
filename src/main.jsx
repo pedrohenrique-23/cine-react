a2103e9d4680e654777d108ed197b8c20d58a2b3
Pedro Henrique <pedrosilva10057@gmail.com>
2025-05-19 23:26:31 -0300

    feat: add favorite context

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { FavoritesProvider } from "./contexts/FavoritesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
