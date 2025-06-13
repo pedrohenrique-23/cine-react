f6c6852460ffa35872222d0ebdd7596f86d2887b
Pedro Henrique <pedrosilva10057@gmail.com>
2025-06-13 11:14:22 -0300

    feat: update login page style

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,3 +1,42 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import FormLogin from "../../components/FormLogin/FormLogin";
+// import styles from "./LoginPage.module.css";
+
+// const LoginPage = () => {
+//   const [formData, setFormData] = useState({ email: "", password: "" });
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log("Login Data:", formData);
+//   };
+
+//   const handleNavigate = () => {
+//     navigate("/register");
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <h1 className={styles.title}>Cine React</h1>
+//       <div className={styles.box}>
+//         <FormLogin
+//           formData={formData}
+//           handleChange={handleChange}
+//           handleSubmit={handleSubmit}
+//           handleNavigate={handleNavigate}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FormLogin from "../../components/FormLogin/FormLogin";
@@ -22,7 +61,8 @@ const LoginPage = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Cine React</h1>
+      <div className={styles.overlay}></div>
+      <h1 className={styles.title}>🎬 Cine React</h1>
       <div className={styles.box}>
         <FormLogin
           formData={formData}
